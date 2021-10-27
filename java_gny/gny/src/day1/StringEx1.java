c3a791d9074c38ecdb5135675cbb7d8d38c490ab
goodaybefore <goodaybefore@gmail.com>
2021-10-27 16:50:48 +0900

    1-5 : 문자열 선언 예제

---
+++ java_gny/gny/src/day1/StringEx1.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 문자열 String
+		 */
+		String str1;
+		String str2 = "Hello";
+		System.out.println(str2);
+		str1 = str2 + "world~";
+		System.out.println(str1);
+	}
+
+}
