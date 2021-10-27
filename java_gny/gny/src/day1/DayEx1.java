52d24b5c583d552121d02d3ca5ce7b26a4d86ae8
goodaybefore <goodaybefore@gmail.com>
2021-10-27 17:30:52 +0900

    1-6 : 예제

---
+++ java_gny/gny/src/day1/DayEx1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class DayEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//폰번호 010 1234 5678 저장해서 확인하기
+		String phone = "01012345671";
+		System.out.println(phone);
+		//성별 저장관리. 알맞은 변수 선언, 초기값으로 남자를 설정
+		boolean isMale = true;//남자 true 여자 false
+
+	}
+
+}
