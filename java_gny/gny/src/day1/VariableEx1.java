2ef7cfa64119a98588830f11796625fbe220f6ef
goodaybefore <goodaybefore@gmail.com>
2021-10-27 16:49:28 +0900

    1-2 : 변수 선언 예제

---
+++ java_gny/gny/src/day1/VariableEx1.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class VariableEx1 {
+
+	public static void main(String[] args) {
+		//한줄주석
+		
+		/*
+		 * 성적을 관리하는 변수를 선언하려 한다.
+		 * 성적은 A, B, C, D, F로 관리할것 
+		 * 성적 관리하기위한 변수를 선언해보세요
+		 */
+		char score;
+		int age = 1;
+		
+		int num2 = 0x11; //16진수
+		System.out.println("num2="+num2);
+		int num3=011;//8진수
+		System.out.println("num3="+num3);
+		int num4=0b11;//2진수
+		System.out.println("num4="+num4);
+	}
+
+}
