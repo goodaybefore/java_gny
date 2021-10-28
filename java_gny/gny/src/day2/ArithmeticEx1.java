b708fe143e6c8aafd7a41e33e0bb545b8837a2cf
goodaybefore <goodaybefore@gmail.com>
2021-10-28 17:45:11 +0900

    Create ArithmeticEx1.java
    산술 연산자

---
+++ java_gny/gny/src/day2/ArithmeticEx1.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class ArithmeticEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=30, num2=7;
+		int add, sub, mul, div, mod;
+		double div2;
+		
+		add = num1+num2;
+		sub = num1-num2;
+		mul = num1*num2;
+		div = num1/num2;
+		div2 = num1/(double)num2;
+		mod = num1%num2;
+		
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
+		System.out.println(div2);
+		System.out.println(mod);
+		
+
+	}
+
+}
