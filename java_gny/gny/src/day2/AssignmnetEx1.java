4df54b1a6bd4b88e6424c3c67cebf4f1a417c8d6
goodaybefore <goodaybefore@gmail.com>
2021-10-28 17:47:04 +0900

    연산자

---
+++ java_gny/gny/src/day2/AssignmnetEx1.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class AssignmnetEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 10, n2 = 20;
+		n1=n2;
+		System.out.println("num1 "+n1+", num2 "+n2);
+		
+		n1=10;
+		n2=20;
+		n2=n1;
+		System.out.println("num1 "+n1+", num2 "+n2);
+		
+		
+		n1=n1+n2;
+		System.out.println("num1 "+n1+", num2 "+n2);
+		
+		
+	}
+
+}
