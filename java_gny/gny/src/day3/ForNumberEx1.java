9199558ebaccf0ee38bdf515337fa6efa1f91303
goodaybefore <goodaybefore@gmail.com>
2021-10-29 17:30:53 +0900

    3-10 : For문으로 숫자 출력

---
+++ java_gny/gny/src/day3/ForNumberEx1.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class ForNumberEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1부터 5까지 출려하는 코드 for
+		
+		for(int i=1;i<6;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
