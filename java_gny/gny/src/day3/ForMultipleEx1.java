15835356fb719da9966b02fcfb87b650709d76af
goodaybefore <goodaybefore@gmail.com>
2021-10-29 17:31:13 +0900

    3-11 : For문으로 구구단출력

---
+++ java_gny/gny/src/day3/ForMultipleEx1.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class ForMultipleEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//7단 출력예시
+		for(int i=1;i<=9;i++) {
+			System.out.println("7 X "+i+" = "+(i*7));
+		}
+
+	}
+
+}
