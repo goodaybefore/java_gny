ab1e41388d7f377dd3827912d788ed24fcbece3e
goodaybefore <goodaybefore@gmail.com>
2021-11-01 13:59:59 +0900

    4-1 : 5부터 1까지 역순출력

---
+++ java_gny/gny/src/day4/ForReverseNumEx1.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class ForReverseNumEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//5부터 1까지 출력하는 예제
+		for(int i=5;i>0;i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
