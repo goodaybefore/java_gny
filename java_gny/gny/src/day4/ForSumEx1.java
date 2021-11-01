5c48e920815f3f200c984ce3be3b9695bd334636
goodaybefore <goodaybefore@gmail.com>
2021-11-01 17:09:20 +0900

    4-7 : ForSum 정수 합 구하기

---
+++ java_gny/gny/src/day4/ForSumEx1.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class ForSumEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 1~10합을 구하는 코드를 FOr로 작성
+		 * 
+		 */
+		
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+			sum += i;
+		}
+		System.out.println("1부터 10까지의 합은 "+sum);
+	}
+
+}
