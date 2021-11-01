ffb848353abc062f0730882a828ea577452ed5f4
goodaybefore <goodaybefore@gmail.com>
2021-11-01 17:10:48 +0900

    4-9 : 무한루프

---
+++ java_gny/gny/src/day4/ForInfiniteLoopEx1.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class ForInfiniteLoopEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 무한루프 : 반복문이 종료되지 않은 상황
+		 * 딱히 안적어두,, 
+		 */
+		
+
+	}
+
+}
