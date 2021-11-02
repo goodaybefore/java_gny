2237a80728e217308ce3b0274284a56bc31adb7a
goodaybefore <goodaybefore@gmail.com>
2021-11-02 16:20:29 +0900

    5-1 : While문으로 Hello 출력

---
+++ java_gny/gny/src/day5/WhileHelloEx1.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class WhileHelloEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		while(a<5) {
+			System.out.println("HELLO");
+			a++;
+		}
+		
+		//while문 이렇게도 쓸 수 있다
+		int i=5;
+		while(i-->0) {
+			System.out.println("Hello");
+		}
+	}
+
+}
