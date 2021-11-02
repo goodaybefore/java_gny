8e96dc0097b9dcfd0276c9a6ce0901f3f8e6788e
goodaybefore <goodaybefore@gmail.com>
2021-11-03 01:08:00 +0900

    5-7 : 배열 Array

---
+++ java_gny/gny/src/day5/ArrayEx1.java
@@ -0,0 +1,24 @@
+package day5;
+
+public class ArrayEx1 {
+
+	public static void main(String[] args) {
+		/* 정수형 배열 선언. 크기는 5개짜리
+		 * 
+		 */
+		//배열 선언 후 나중에 배열을 생성하는 방법
+		int array [];
+		array = new int[5];
+		int[] array2 = new int[5]; 
+		
+		int array3[];
+		int[] array4;
+		array3 = new int[5];
+		array4 = new int[5];
+		
+		int size = 5;
+		int array5[] = new int[size];
+
+	}
+
+}
---
+++ java_gny/gny/src/day5/ArrayEx2.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class ArrayEx2 {
+
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		//배열 arr의 0번지에 1
+		arr[0] = 1;
+		System.out.println(arr[0]);
+		arr[1] = 2;
+		System.out.println(arr[1]);
+		arr[2] = 3;
+		System.out.println(arr[2]);
+		arr[3] = 4;
+		System.out.println(arr[3]);
+		arr[4] = 5;
+		System.out.println(arr[4]);
+	}
+
+}
