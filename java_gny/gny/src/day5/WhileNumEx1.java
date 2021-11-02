6694acfb4d9a20612ccb79a536247e417e0e6a22
goodaybefore <goodaybefore@gmail.com>
2021-11-02 16:21:00 +0900

    5-2 : While문으로 숫자 출력

---
+++ java_gny/gny/src/day5/WhileNumEx1.java
@@ -0,0 +1,22 @@
+package day5;
+
+public class WhileNumEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1-5 출력하는 코드
+		for(int i=1;i<=5;i++) {
+			System.out.println(i+" ");
+		}
+		
+		int i = 1;
+		while(i<=5) {
+			System.out.println(i +" ");
+			i++;
+		}
+		
+		
+
+	}
+
+}
