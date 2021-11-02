adccca4ce43907a5fff710e4f630c7da344f6756
goodaybefore <goodaybefore@gmail.com>
2021-11-02 16:23:27 +0900

    5-5 : While으로 7단 출력

---
+++ java_gny/gny/src/day5/WhileMultipleTableEx1.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class WhileMultipleTableEx1 {
+
+	public static void main(String[] args) {
+		/* while 문을 이용해 7단을 출력하는 코드작성
+		 * 
+		 */
+		
+		int i=0;
+		while(i<9) {
+			i++;
+			System.out.println("7 * "+i+" = "+(7*i));
+		}
+
+	}
+
+}
