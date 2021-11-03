2cfdbaddb6fca8c8839314e9dff5fcaeedd2e6dd
goodaybefore <goodaybefore@gmail.com>
2021-11-03 17:14:07 +0900

    6-3 : 배열의 길이

---
+++ java_gny/gny/src/day6/ArrayLengthEx1.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class ArrayLengthEx1 {
+
+	public static void main(String[] args) {
+		/* 
+		 * 
+		 */
+		
+		int [] arr = new int[5];
+		System.out.println("배열의 길이 : "+arr.length);
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("arr["+i+"] = " + arr[i] );
+		}
+
+	}
+
+}
