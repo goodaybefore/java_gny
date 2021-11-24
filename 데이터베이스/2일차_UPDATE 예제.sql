dc56af70ef585f57ae9e8df364f7e9c8d19ce412
goodaybefore <goodaybefore@gmail.com>
2021-11-24 22:49:04 +0900

    21-1 : 2일차 SQL 수정

--- 데이터베이스/2일차_UPDATE 예제.sql
+++ 데이터베이스/2일차_UPDATE 예제.sql
@@ -1,3 +1,5 @@
+SELECT * FROM student;
+TRUNCATE student;
 UPDATE student 
 	SET
 		st_id = 'nayoung2',
@@ -11,4 +13,11 @@ UPDATE student
         st_pw = 'ny72361'
 	WHERE
 		st_name='권나영';
+UPDATE subject
+	SET
+    su_schedule = "월요일 1,2교시",
+    su_class_num = 3
+    WHERE
+    su_num=1
+    ;
 SELECT * FROM university_ny.student;
