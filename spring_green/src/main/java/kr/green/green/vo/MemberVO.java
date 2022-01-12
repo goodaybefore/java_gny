bfd989cdc7eb1fced4685da23f6bf386cf2fc8ff
goodaybefore <goodaybefore@gmail.com>
2022-01-13 03:25:28 +0900

    스프링 복습 : 롬복 추가2

--- spring_green/src/main/java/kr/green/green/vo/MemberVO.java
+++ spring_green/src/main/java/kr/green/green/vo/MemberVO.java
@@ -14,8 +14,15 @@ public class MemberVO {
 	private String me_pw;
 	private String me_name;
 	private String me_gender;
+	//sette의 매개변수에 문자열이 오는 경우 이렇게 지정
+	//getter는 그냥 지정(BoardVO참고)
+	
+	//setter가 필요한 경우 : 화면에서 정보를 전달받을 때.
+	//ex.del_date, up_date; => db에 자동으로 들어가기 때문에 data type 이 필요없음
+	//memverVo에서 필요한 이유 : 화면에서 전달해주는건 문자열이기 때문에 DateTimeFormat이 필요 
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date me_birth;
+	
 	private String me_address;
 	private String me_phone;
 	
